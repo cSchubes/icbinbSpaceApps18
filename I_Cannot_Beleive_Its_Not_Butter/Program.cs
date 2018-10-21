be26fac057b362865f8ad6336e95275e155ca1fc
Lakiel Wade <lakielw@hotmail.com>
2018-10-20 21:41:33 -0400

    more coding

--- I_Cannot_Beleive_Its_Not_Butter/Program.cs
+++ I_Cannot_Beleive_Its_Not_Butter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using IronPython.Hosting;
 
 namespace I_Cannot_Beleive_Its_Not_Butter
 {
@@ -7,6 +10,25 @@ namespace I_Cannot_Beleive_Its_Not_Butter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            List<ImageProcessors> allim = new List<ImageProcessors>();
+            Image i1 = Image.FromFile("cat1.jpeg");
+            Image i2 = Image.FromFile("cat2.jpg");
+
+            allim.Add(new ImageProcessors(1, i1));
+            allim.Add(new ImageProcessors(2, i2));
+
+            List<ImageObj> imgObj = new List<ImageObj>();
+            imgObj.AddRange(allim);
+
+            var ipy = Python.CreateRuntime();
+            dynamic test = ipy.UseFile("ReturnDamaged.py");
+
+            var resultImage = test.feed_model(imgObj);
+
+            Console.WriteLine($"{imgObj[0].ID}");
+            Console.WriteLine($"{imgObj[1].ID}");
+
         }
     }
 }
