91e546cee38475ef32e1c4810b62578f4eeffc4e
sherborneseaborne <nicholaswest@utexas.edu>
2018-10-20 21:32:31 -0400

    Add files via upload

---
+++ I_Cannot_Beleive_Its_Not_Butter/ReturnDamaged.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def feed_model(image_data):
+    
+    PATH = '../CNN' ### THIS NEEDS TO BE CHANGED
+    
+    model = TheModelClass()
+    model.load_state_dict(torch.load(PATH))
+
+    model.eval()
+
+    torch_data = torch.from_numpy(image_data)
+    
+    output = model(torch_data)
+    indices = [i for i, n in enumerate(output) if n == 0] # List comprehension
+
+    damaged_pics = [torch_data[i] for i in indices]    
+
+    return damaged_pics
+
+
+if __name__ == "__main__":
+    feed_model()
